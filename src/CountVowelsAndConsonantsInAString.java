15fc642389539d480437421516aee92832a52d9a
marufremoncse <marufremon.cse14.2@gmail.com>
2024-09-02 18:50:30 +0600

    Count Vowels and Consonants in a String

---
+++ src/CountVowelsAndConsonantsInAString.java
@@ -0,0 +1,22 @@
+public class CountVowelsAndConsonantsInAString {
+    //Count Vowels and Consonants in a String
+    public static void solution(String input) {
+        int vc = 0, cc = 0;
+
+        for(char c: input.toLowerCase().toCharArray()){
+            if(Character.isLetter(c)){
+                if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+                    vc++;
+                } else{
+                    cc++;
+                }
+            }
+        }
+        System.out.println(String.format("Vowels : %d, Consonants: %d", vc, cc));
+    }
+
+    public static void main(String[] args) {
+        solution("Hello World");
+        solution("Java Programming");
+    }
+}
